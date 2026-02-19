21b7dd8abb78d05fabca63d712ae3995733bf051
K K SUKANYA <kksukanya06@gmail.com>
2026-02-20 00:48:50 +0530

    updated api file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // backend URL
+  baseURL: '/api',   // IMPORTANT: no localhost
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
-  if(token) req.headers.Authorization = `Bearer ${token}`;
+  if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
 });
 
